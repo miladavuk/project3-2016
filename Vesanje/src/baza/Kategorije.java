97e0febc598e390d7c8c11821d0c9bc1e6cb4674
lukabaljak <luk.abaljak@hotmail.com>
2016-04-19 21:01:48 +0200

    U klasu Kategorija je dodat string brendovi i filmovi sa 20 pojmova.

---
+++ Vesanje/src/baza/Kategorije.java
@@ -0,0 +1,12 @@
+package baza;
+
+public class Kategorije {
+
+	String[] filmovi = new String[] { "The Pink Panther", "Minions", "Despicable Me", "Scream", "Hunger Games",
+			"Interstellar", "Schindler's List", "Pirats Of The Carribian", "Madagascar", "Frozen", "Hobbit",
+			"The Lord Of The Rings", "Adaptation", "Dead Poets Society", "Hook", "Spiderman", "Avengers", "Avatar",
+			"Titanic", "Finding Neverland" };
+	String[] brendovi = new String[] { "Coca-Cola", "Orbit", "Bershka", "Converse", "Nike", "Rebook", "Celvin Klein",
+			"Java", "Nokia", "Microsoft", "Pionir", "Marlboro", "Miflord", "Nivea", "Dove", "Gorenje", "Banini",
+			"Bambi", "Toshiba", "Genius" };
+}
