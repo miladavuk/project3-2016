20dbd93f78fbd6fca73aa9b484f92837c79e6dd6
miladavuk <miladavuk@gmail.com>
2016-04-19 20:36:22 +0200

    Dodati nizovi Stringova knjige i drzave u klasu kategorije. Svaki niz sadrzi 20 Stringova.

---
+++ Vesanje/src/baza/Kategorije.java
@@ -0,0 +1,29 @@
+package baza;
+
+public class Kategorije {
+	
+	private String[] knjige = new String[] {"Gone with the wind","A thousand splendid suns",
+			"The kite runner","Harry Potter and the philosopher's stone",
+			"The adventures of Huckleberry Finn","The great Gatsby",
+			"The brothers Karamazov","One hundred years of solitude",
+			"The catcher in the Rye","Pride and prejudice",
+			"To kill a mockingbird","Wuthering heights",
+			"Crime and punishment","The old man and the sea",
+			"The picture of Dorian Grey","Chronicles of Narnia",
+			"Twenty thousand leagues under the sea","The three musketeers",
+			"The count of Monte Cristo","The adventures of Sherlock Holmes"};
+	private String[] drzave = new String[]{"Czech Republic","Dominican republic",
+			"Republic of Djibouti","Republic of the Congo",
+			"Equatorial Guinea","Guadeloupe",
+			"Kyrgyzstan","Luxembourg",
+			"Mozambique","Netherlands",
+			"Philippines","Saudi Arabia",
+			"Swaziland","Trinidad and Tobago",
+			"United Arab Emirates","Antigua and Barbuda",
+			"Bosnia and Herzegovina","Burkina Faso",
+			"Botswana","El Salvador"};
+	
+	
+			
+
+}
