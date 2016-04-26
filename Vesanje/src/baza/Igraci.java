ec9e9c7faa59d64440764fd0575be0e676592fca
lukabaljak <luk.abaljak@hotmail.com>
2016-04-26 13:00:08 +0200

    Dodat je JavaDoc za sve atribute i metode klase Igraci.

--- Vesanje/src/baza/Igraci.java
+++ Vesanje/src/baza/Igraci.java
@@ -7,59 +7,110 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.LinkedList;
 
-public class Igraci implements Serializable{
+/*
+ * U ovoj klasi su implementirane osnovne metode za serijalizaciju i deserijalizaciju podataka o igracima.
+ * Pored toga, ova klasa sluzi i za pretrazivanje i pronalazenje trazenog igraca.
+ */
+
+public class Igraci implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	/*
+	 * Lista koja sadrzi igrace.
+	 */
 	private LinkedList<Igrac> igraci;
-	
-	public Igraci(){
+
+	/*
+	 * Konstruktor inicijalizuje listu igraca i deserijalizuje sve prethodno
+	 * serijalizovane podatke o igracima.
+	 */
+	public Igraci() {
 		igraci = new LinkedList<>();
 		deserijalizujIgrace();
 	}
-	
-	public boolean daLiPostoji(String ime){
+
+	/*
+	 * @param ime Predstavlja ime igraca.
+	 * 
+	 * @return Metoda proverava da li u listi igraca postoji igrac kod kog je
+	 * vrednost atributa ime jednak vrednosti koja pokusava da se unese kao
+	 * parametear. Ako jeste, metoda vraca true, a u suprotnom, vraca false.
+	 */
+	public boolean daLiPostoji(String ime) {
 		for (Igrac igrac : igraci) {
-			if(igrac.getIme().equals(ime))return true;
+			if (igrac.getIme().equals(ime))
+				return true;
 		}
 		return false;
 	}
-	
-	public void dodajIgraca(String ime){
+	/*
+	 * Metoda dodaje novog igraca u listu cije se ime unosi kao parametar.
+	 */
+
+	public void dodajIgraca(String ime) {
 		igraci.add(new Igrac(ime));
 	}
 
-	
-	public void serijalizujIgrace(){
-		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Igraci.ser"))){
+	/*
+	 * Metoda serijalizuje podatke o svim igracima iz liste igraca.
+	 * 
+	 * @throws Exception Metoda baca izuzetak u slucaju neuspesnog pokusaja
+	 * serijlizacije podataka o igracima.
+	 */
+	public void serijalizujIgrace() {
+		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("Igraci.ser"))) {
 			out.writeObject(igraci);
 		} catch (Exception e) {
 			System.out.println("Nije uspelo serijalizovanje!");
 		}
 
 	}
-	
-	public void deserijalizujIgrace(){
-		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("Igraci.ser"))){
-			igraci.addAll((LinkedList<Igrac>)in.readObject());			
+
+	/*
+	 * Metoda deserijalizuje sve podatke o igracima iz liste.
+	 * 
+	 * @throws Exception Metoda baca izuzetak u slucaju neuspesnog pokusaja
+	 * deserijalizacije podataka o igracima.
+	 */
+	public void deserijalizujIgrace() {
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("Igraci.ser"))) {
+			igraci.addAll((LinkedList<Igrac>) in.readObject());
 		} catch (Exception e) {
 			System.out.println("Nije uspelo ocitavanje!");
 		}
 	}
-	
-	public int vratiIndexIgraca(String ime){
+
+	/*
+	 * @param ime Metodi se prosledjuje ime igraca za kojim se traga.
+	 * 
+	 * @return Vraca se vrednost indeksa igraca u listi igraca.
+	 * 
+	 * U slucaju da igrac sa tim imenom ne postoji u listi, metoda vraca
+	 * vrednost -1, sto oznacava da nije unet jos u listu.
+	 */
+	public int vratiIndexIgraca(String ime) {
 		for (int i = 0; i < igraci.size(); i++) {
-			if(igraci.get(i).getIme().equals(ime)) return i;
+			if (igraci.get(i).getIme().equals(ime))
+				return i;
 		}
 		return -1;
 	}
-	
-	public Igrac vratiIgraca(int index){
+
+	/*
+	 * @param index Metodi se prosledjuje indeks igraca za kojim se traga.
+	 * 
+	 * @return Metoda vraca igraca iz liste kojem pripada taj indeks.
+	 */
+	public Igrac vratiIgraca(int index) {
 		return igraci.get(index);
 	}
-	
-	public void ispisiIgrace(){
+
+	/*
+	 * Metoda ispisuje sva imena igraca iz liste.
+	 */
+	public void ispisiIgrace() {
 		for (Igrac igrac : igraci) {
 			System.out.println(igrac.getIme());
 		}
