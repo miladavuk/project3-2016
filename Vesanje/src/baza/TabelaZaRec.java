9094fb3b3a1839d198138cf594edd8390cee82f3
lukabaljak <luk.abaljak@hotmail.com>
2016-04-26 13:10:52 +0200

    Dodat JavaDoc za klasu TabelaZaRec.

--- Vesanje/src/baza/TabelaZaRec.java
+++ Vesanje/src/baza/TabelaZaRec.java
@@ -1,39 +1,51 @@
 package baza;
 
 import javax.swing.table.AbstractTableModel;
-
+/*
+ * Klasa predtsavlja tabelu u kojoj ce se ispisivati pojam za kojim igrac traga. 
+ */
 
 public class TabelaZaRec extends AbstractTableModel {
-	
+	/*
+	 * Atribut oznacava niz karaktera koji predstavljaju pojam koji igrac treba
+	 * da pogodi.
+	 */
 	private char[] recZaPrikazNiz;
-	
+
+	/*
+	 * @return Metoda vraca pomenuti niz karaktera.
+	 */
 	public char[] getRecZaPrikazNiz() {
 		return recZaPrikazNiz;
 	}
 
-
+	/*
+	 * @param recZaPrikaz Metodi se prosledjuje vrednost reci.
+	 * 
+	 * Konstruktor postavlja vrednost atributa za rec na unetu vrednost. Potom
+	 * se inicijalizuje tabela.
+	 */
 	public TabelaZaRec(char[] recZaPrikaz) {
-//		recZaPrikazNiz = new char[recZaPrikaz.length];
+		// recZaPrikazNiz = new char[recZaPrikaz.length];
 		recZaPrikazNiz = recZaPrikaz;
 		fireTableDataChanged();
 	}
 
-	
 	@Override
 	public int getColumnCount() {
-		
+
 		return recZaPrikazNiz.length;
 	}
 
 	@Override
 	public int getRowCount() {
-		
+
 		return 1;
 	}
 
 	@Override
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		
+
 		return recZaPrikazNiz[columnIndex];
 	}
 
