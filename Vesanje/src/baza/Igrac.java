0110a88aec34531278dbe682f16993ba651167cd
lukabaljak <luk.abaljak@hotmail.com>
2016-04-26 12:22:05 +0200

    Dodat je JavaDoc za sve metode i atribute klase Igrac, kao i za sve atribute i get metode klase Kategorije.

--- Vesanje/src/baza/Igrac.java
+++ Vesanje/src/baza/Igrac.java
@@ -3,19 +3,38 @@ package baza;
 import java.io.Serializable;
 import java.util.LinkedList;
 
-public class Igrac implements Serializable{
+/*
+ * 		Klasa sadrzi osnovne informacije o igracu. 
+ * 		Svi podaci o igracima se serijalizuju.
+ * 		Sadrzi informacije o tome koje pojmove iz odredjene kategorije je igrac uspeo da pogodi.
+ */
+
+public class Igrac implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	/*
+	 * Atribut oznacava ime igraca.
+	 */
 	private String ime;
+
+	/*
+	 * Ovi atributi predstavljaju listu indeksa pojmova iz nizova kategorija,
+	 * koje je igrac uspeo da pogodi.
+	 */
 	private LinkedList<Integer> knjige;
 	private LinkedList<Integer> drzave;
 	private LinkedList<Integer> filmovi;
 	private LinkedList<Integer> brendovi;
 	private LinkedList<Integer> poznateLicnosti;
 	private LinkedList<Integer> istorijskeLicnosti;
-	
+
+	/*
+	 * @param ime Konstruktor postavlja vrednost polja ime na vrednost unetog
+	 * atributa i inicijalizuje gore pomenute liste indeksa.
+	 */
+
 	public Igrac(String ime) {
 		this.ime = ime;
 		knjige = new LinkedList<>();
@@ -25,56 +44,107 @@ public class Igrac implements Serializable{
 		poznateLicnosti = new LinkedList<>();
 		istorijskeLicnosti = new LinkedList<>();
 	}
-	
+
+	/*
+	 * @return Metoda vraca vrednost atributa ime.
+	 */
 	public String getIme() {
 		return ime;
 	}
 
+	/*
+	 * @return Metoda vraca listu indeksa pogodjenih pojmova za kategoriju
+	 * knjige.
+	 */
 	public LinkedList<Integer> getKnjige() {
 		return knjige;
 	}
 
+	/*
+	 * @return Metoda vraca listu indeksa pogodjenih pojmova za kategoriju
+	 * drzave.
+	 */
 	public LinkedList<Integer> getDrzave() {
 		return drzave;
 	}
 
+	/*
+	 * @return Metoda vraca listu indeksa pogodjenih pojmova za kategoriju
+	 * filmovi.
+	 */
 	public LinkedList<Integer> getFilmovi() {
 		return filmovi;
 	}
 
+	/*
+	 * @return Metoda vraca listu indeksa pogodjenih pojmova za kategoriju
+	 * brendovi.
+	 */
 	public LinkedList<Integer> getBrendovi() {
 		return brendovi;
 	}
 
+	/*
+	 * @return Metoda vraca listu indeksa pogodjenih pojmova za kategoriju
+	 * poznate licnosti.
+	 */
 	public LinkedList<Integer> getPoznateLicnosti() {
 		return poznateLicnosti;
 	}
 
+	/*
+	 * @return Metoda vraca listu indeksa pogodjenih pojmova za kategoriju
+	 * istorijske licnosti.
+	 */
 	public LinkedList<Integer> getIstorijskeLicnosti() {
 		return istorijskeLicnosti;
 	}
-	
-	public void ubaciKnjigu(int index){
+
+	/*
+	 * @param index Indeks pogodjenog pojma iz kategorije knjige se ubacuje u
+	 * listu indeksa za tu kategoriju.
+	 */
+	public void ubaciKnjigu(int index) {
 		knjige.add(new Integer(index));
 	}
-	
-	public void ubaciDrzavu(int index){
+
+	/*
+	 * @param index Indeks pogodjenog pojma iz kategorije drzave se ubacuje u
+	 * listu indeksa za tu kategoriju.
+	 */
+	public void ubaciDrzavu(int index) {
 		drzave.add(new Integer(index));
 	}
-	
-	public void ubaciFilm(int index){
+
+	/*
+	 * @param index Indeks pogodjenog pojma iz kategorije filmovi se ubacuje u
+	 * listu indeksa za tu kategoriju.
+	 */
+	public void ubaciFilm(int index) {
 		filmovi.add(new Integer(index));
 	}
-	
-	public void ubaciBrend(int index){
+
+	/*
+	 * @param index Indeks pogodjenog pojma iz kategorije brendovi se ubacuje u
+	 * listu indeksa za tu kategoriju.
+	 */
+	public void ubaciBrend(int index) {
 		brendovi.add(new Integer(index));
 	}
-	
-	public void ubaciIstorijskuLicnost(int index){
+
+	/*
+	 * @param index Indeks pogodjenog pojma iz kategorije istorijske licnosti se
+	 * ubacuje u listu indeksa za tu kategoriju.
+	 */
+	public void ubaciIstorijskuLicnost(int index) {
 		istorijskeLicnosti.add(new Integer(index));
 	}
-	
-	public void ubaciPoznatuLicnost(int index){
+
+	/*
+	 * @param index Indeks pogodjenog pojma iz kategorije poznate licnosti se
+	 * ubacuje u listu indeksa za tu kategoriju.
+	 */
+	public void ubaciPoznatuLicnost(int index) {
 		poznateLicnosti.add(new Integer(index));
 	}
 }
--- Vesanje/src/baza/Kategorije.java
+++ Vesanje/src/baza/Kategorije.java
@@ -1,83 +1,120 @@
 
 package baza;
 
+/*
+ *   U ovoj klasi se nalaze kategorije pojmova koje mogu da se odaberu, kao i 20 pojmova po svakoj kategoriji.
+ */
 public class Kategorije {
-	
-	private String[] knjige = new String[] {"Gone with the wind","A thousand splendid suns",
-			"The kite runner","Harry Potter and the philosopher's stone",
-			"The adventures of Huckleberry Finn","The great Gatsby",
-			"The brothers Karamazov","One hundred years of solitude",
-			"The catcher in the Rye","Pride and prejudice",
-			"To kill a mockingbird","Wuthering heights",
-			"Crime and punishment","The old man and the sea",
-			"The picture of Dorian Grey","Chronicles of Narnia",
-			"Twenty thousand leagues under the sea","The three musketeers",
-			"The count of Monte Cristo","The adventures of Sherlock Holmes"};
-	private String[] drzave = new String[]{"Czech Republic","Dominican republic",
-			"Republic of Djibouti","Republic of the Congo",
-			"Equatorial Guinea","Guadeloupe",
-			"Kyrgyzstan","Luxembourg",
-			"Mozambique","Netherlands",
-			"Philippines","Saudi Arabia",
-			"Swaziland","Trinidad and Tobago",
-			"United Arab Emirates","Antigua and Barbuda",
-			"Bosnia and Herzegovina","Burkina Faso",
-			"Botswana","El Salvador"};
-	
+	/*
+	 * Atribut predstavlja niz od 20 pojmova za kategoriju knjge.
+	 */
+	private String[] knjige = new String[] { "Gone with the wind", "A thousand splendid suns", "The kite runner",
+			"Harry Potter and the philosopher's stone", "The adventures of Huckleberry Finn", "The great Gatsby",
+			"The brothers Karamazov", "One hundred years of solitude", "The catcher in the Rye", "Pride and prejudice",
+			"To kill a mockingbird", "Wuthering heights", "Crime and punishment", "The old man and the sea",
+			"The picture of Dorian Grey", "Chronicles of Narnia", "Twenty thousand leagues under the sea",
+			"The three musketeers", "The count of Monte Cristo", "The adventures of Sherlock Holmes" };
+	/*
+	 * Atribut predstavlja niz od 20 pojmova za kategoriju drzave.
+	 */
+	private String[] drzave = new String[] { "Czech Republic", "Dominican republic", "Republic of Djibouti",
+			"Republic of the Congo", "Equatorial Guinea", "Guadeloupe", "Kyrgyzstan", "Luxembourg", "Mozambique",
+			"Netherlands", "Philippines", "Saudi Arabia", "Swaziland", "Trinidad and Tobago", "United Arab Emirates",
+			"Antigua and Barbuda", "Bosnia and Herzegovina", "Burkina Faso", "Botswana", "El Salvador" };
+	/*
+	 * Atribut predstavlja niz od 20 pojmova za kategoriju filmovi.
+	 */
 	private String[] filmovi = new String[] { "The Pink Panther", "Minions", "Despicable Me", "Scream", "Hunger Games",
 			"Interstellar", "Schindler's List", "Pirats Of The Carribian", "Madagascar", "Frozen", "Hobbit",
 			"The Lord Of The Rings", "Adaptation", "Dead Poets Society", "Hook", "Spiderman", "The Avengers", "Avatar",
 			"Titanic", "Finding Neverland" };
-	private String[] brendovi = new String[] { "Coca Cola", "Orbit", "Bershka", "Converse", "Nike", "Rebook", "Celvin Klein",
-			"Java", "Nokia", "Microsoft", "Pionir", "Marlboro", "Miflord", "Nivea", "Dove", "Gorenje", "Banini",
-			"Bambi", "Toshiba", "Genius" };
-			
-	private String[] poznateLicnosti = new String[]{
-			"Rafael Nadal", "Rihanna", "Brad Pitt", "Leonardo DiCaprio", "Tom Cruise", "Barak Obama","Emma Watson", 
-			"Johnny Depp", "Morgan Freeman", "Michael Jordan", "Kobe Bryant", "LeBron James", "Madonna","Stephen King",
-			"Michael Jackson", "Jennifer Lopez", "Bill Gates", "Paulo Coelho", "Joanne Kathleen Rowling", "George RR Martin"
-			};
-	private String[] istorijskeLicnosti = new String[]{
-			"William Shakespeare", "Abraham Lincoln", "George Washington", "Adolf Hitler", "Aristotle", "Charles Darwin",
-			"Karl Marx", "Julius Caesar", "Albert Einstein", "Christopher Columbus", "Isaac Newton", "Wolfgang Amadeus Mozart",
-			"Leonardo da Vinci", "Charles Dickens", "Winston Churchill", "Friedrich Nietzsche", "Sigmund Freud", "Johann Sebastian Bach",
-			"Galileo Galilei", "Adam Smith"
-	};
+	/*
+	 * Atribut predstavlja niz od 20 pojmova za kategoriju brendovi.
+	 */
+	private String[] brendovi = new String[] { "Coca Cola", "Orbit", "Bershka", "Converse", "Nike", "Rebook",
+			"Celvin Klein", "Java", "Nokia", "Microsoft", "Pionir", "Marlboro", "Miflord", "Nivea", "Dove", "Gorenje",
+			"Banini", "Bambi", "Toshiba", "Genius" };
+	/*
+	 * Atribut predstavlja niz od 20 pojmova za kategoriju poznate licnosti.
+	 */
+	private String[] poznateLicnosti = new String[] { "Rafael Nadal", "Rihanna", "Brad Pitt", "Leonardo DiCaprio",
+			"Tom Cruise", "Barak Obama", "Emma Watson", "Johnny Depp", "Morgan Freeman", "Michael Jordan",
+			"Kobe Bryant", "LeBron James", "Madonna", "Stephen King", "Michael Jackson", "Jennifer Lopez", "Bill Gates",
+			"Paulo Coelho", "Joanne Kathleen Rowling", "George RR Martin" };
+	/*
+	 * Atribut predstavlja niz od 20 pojmova za kategoriju istorijske licnosti.
+	 */
+	private String[] istorijskeLicnosti = new String[] { "William Shakespeare", "Abraham Lincoln", "George Washington",
+			"Adolf Hitler", "Aristotle", "Charles Darwin", "Karl Marx", "Julius Caesar", "Albert Einstein",
+			"Christopher Columbus", "Isaac Newton", "Wolfgang Amadeus Mozart", "Leonardo da Vinci", "Charles Dickens",
+			"Winston Churchill", "Friedrich Nietzsche", "Sigmund Freud", "Johann Sebastian Bach", "Galileo Galilei",
+			"Adam Smith" };
+
+	/*
+	 * @return Metoda vraca niz pojmova za kategoriju knjige.
+	 */
+
 	public String[] getKnjige() {
 		return knjige;
 	}
+
 	public void setKnjige(String[] knjige) {
 		this.knjige = knjige;
 	}
+
+	/*
+	 * @return Metoda vraca niz pojmova za kategoriju drzave.
+	 */
 	public String[] getDrzave() {
 		return drzave;
 	}
+
 	public void setDrzave(String[] drzave) {
 		this.drzave = drzave;
 	}
+
+	/*
+	 * @return Metoda vraca niz pojmova za kategoriju filmovi.
+	 */
 	public String[] getFilmovi() {
 		return filmovi;
 	}
+
 	public void setFilmovi(String[] filmovi) {
 		this.filmovi = filmovi;
 	}
+
+	/*
+	 * @return Metoda vraca niz pojmova za kategoriju brendovi.
+	 */
 	public String[] getBrendovi() {
 		return brendovi;
 	}
+
 	public void setBrendovi(String[] brendovi) {
 		this.brendovi = brendovi;
 	}
+
+	/*
+	 * @return Metoda vraca niz pojmova za kategoriju poznate licnosti.
+	 */
 	public String[] getPoznateLicnosti() {
 		return poznateLicnosti;
 	}
+
 	public void setPoznateLicnosti(String[] poznateLicnosti) {
 		this.poznateLicnosti = poznateLicnosti;
 	}
+
+	/*
+	 * @return Metoda vraca niz pojmova za kategoriju istorijske licnosti.
+	 */
 	public String[] getIstorijskeLicnosti() {
 		return istorijskeLicnosti;
 	}
+
 	public void setIstorijskeLicnosti(String[] istorijskeLicnosti) {
 		this.istorijskeLicnosti = istorijskeLicnosti;
 	}
-	
+
 }
